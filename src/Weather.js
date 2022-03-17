d243fd1e028f7af4ee4be0955af2620bf0e28dfd
eweymuller <eweymuller@gmail.com>
2022-03-17 13:44:56 -0700

    Added axios integration so it shows the weather plus loading icon

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import LoadingIcons from "react-loading-icons";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C `
+    );
+  }
+  let apiKey = "7633347349ec94a368e4a15d93744b30";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <LoadingIcons.Puff />;
+}
